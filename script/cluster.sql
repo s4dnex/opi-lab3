a728e89e0cefecf9081604ea2dcde3236fe57d43
sadnex <s4dnex@gmail.com>
2026-04-15 16:16:21 +0300

    revert: remove files from dop task

--- script/cluster.sql
+++
@@ -1,9 +0,0 @@
--- Use only one time for "initialization" of cluster
-
-CREATE INDEX CONCURRENTLY IF NOT EXISTS points_request_time_desc_idx
-    ON points (request_time DESC);
-
-ALTER TABLE points
-    CLUSTER ON points_request_time_desc_idx;
-
-CLUSTER points USING points_request_time_desc_idx;
